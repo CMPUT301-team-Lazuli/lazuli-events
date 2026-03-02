3040810f078e3805d3a6e38aec64bafe9aa3a944
mwalters799 <mwalters@ualberta.ca>
2026-03-02 13:01:50 -0700

    doc: fixed javadoc for Profile class

--- lazuli-events/app/src/main/java/com/example/lazuli_events/Profile.java
+++ lazuli-events/app/src/main/java/com/example/lazuli_events/Profile.java
@@ -3,8 +3,12 @@ package com.example.lazuli_events;
 import java.util.ArrayList;
 
 /**
- * This is a class representing a user profile. It stores a user's
- * profile data, event registration history, and notification preference.
+ * This is a class representing a user profile. It stores a user's:
+ * <li>profile data (name, email, phone number, deviceId)</li>
+ * <li>event registration history, as a String ArrayList</li>
+ * <li>notification preference</li>
+ * These elements can be retrieved and updated, and the Profile can check if an event id is in
+ * its event history.
  */
 public class Profile {
     private String name;
@@ -24,59 +28,122 @@ public class Profile {
         this.eventIds = eventIds;
     }
 
+    /**
+     * Gets a profile's full name.
+     * @return  the profile's name as a String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets a profile's name to a new one.
+     * @param name  the new name for the selected profile
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets a profile's email.
+     * @return  the profile's email as a String
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets a profile's email to a new one.
+     * @param email the new email for the selected profile
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets a profile's phone number.
+     * @return  the profile's phone number as a String
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Sets the profile's phone number to a new one.
+     * @param phone the new phone number for the selected profile
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    /**
+     * Gets a profile's device id.
+     * @return  the profile's device id as a String
+     */
     public String getDeviceId() {
         return deviceId;
     }
 
+    /**
+     * Sets the profile's device id to a new one.
+     * @param deviceId  the new deviceId for the selected profile
+     */
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
     }
 
+    /**
+     * Gets a profile's notification preference.
+     * @return the profile's notification preference
+     */
     public String getNotifPref(){
         return notifPref;
     }
+
+    /**
+     * Sets the profile's device id to a new one, throws an error if new notification preference
+     * is not one of the correct ones.
+     * @throws java.io.IOException  If an input is not a valid notification preference to have.
+     * @param notifPref the new deviceId for the selected profile
+     */
     public void setNotifPref(String notifPref){
+        //TODO: assert notifPref is a valid option
+
+        //update notification preference
         this.notifPref = notifPref;
     }
 
+    /**
+     * Gets a profile's event history.
+     * @return  the profile's ArrayList of event IDs (as Strings)
+     */
     public ArrayList<String> getEventIds(){
         return eventIds;
     }
+
+    /**
+     * Adds a specific eventID to a profile's event history only if the eventID is not already in
+     * the profile's event history.
+     * @param eventId the event ID to be added
+     */
     public void addEventId(String eventId){
+        //TODO: Throw error if event is already in eventIds
+
+        //Add event
         eventIds.add(eventId);
     }
 
+    /** Checks if an eventId is in a profile's event history.
+     * @param eventId: the event ID to be checked
+     * @return boolean: true if the eventID is in its history, false if not
+     */
     public boolean hasEventId(String eventId){
         return eventIds.contains(eventId);
     }
 
     /**
      * Removes a given event id from a Profile.
-     * @param eventId the event id to remove from the Profile
+     * @param eventId   the event id to remove from the Profile
      * @throws IllegalArgumentException when the eventId to be deleted
      * is not in the Profile's eventIds
      */
