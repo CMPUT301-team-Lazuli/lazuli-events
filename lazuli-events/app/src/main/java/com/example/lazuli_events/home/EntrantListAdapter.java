48ddd13514d9bba6585b6f54f15b2ab94a6f29b8
kieetttt <144357977+kieetttt@users.noreply.github.com>
2026-03-10 17:11:21 -0600

    added drop button

--- lazuli-events/app/src/main/java/com/example/lazuli_events/home/EntrantListAdapter.java
+++ lazuli-events/app/src/main/java/com/example/lazuli_events/home/EntrantListAdapter.java
@@ -6,10 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.example.lazuli_events.R;
+
+import com.example.lazuli_events.MainActivity;
 
 import java.util.ArrayList;
 
@@ -20,8 +24,16 @@ import javax.annotation.Nullable;
  * It sets the list item's layout to display the attributes of each entrant object.
  */
 public class EntrantListAdapter extends ArrayAdapter<String> {
+
+    private boolean showDropButton = false; // determine if drop button should be visible
     public EntrantListAdapter(Context context, ArrayList<String> entrantList) {
         super(context, 0, entrantList);
+    }
+
+    // updates the adapter to show or hide the drop button
+    public void setShowDropButton(boolean show) {
+        this.showDropButton = show;
+        notifyDataSetChanged();
     }
 
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -39,6 +51,33 @@ public class EntrantListAdapter extends ArrayAdapter<String> {
         if (entrantText != null) {
             textView.setText(entrantText);
         }
+
+        // get the drop button
+        ImageButton dropButton = itemView.findViewById(R.id.entrant_drop_button);
+        if (showDropButton) {
+            dropButton.setVisibility(View.VISIBLE);
+        } else {
+            dropButton.setVisibility(View.GONE);
+        }
+
+        // handle drop button clicks
+        dropButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                // get the activity to access the global database
+                MainActivity mainActivity = (MainActivity) getContext();
+
+                // remove from Firestore
+                mainActivity.firebaseDB.dropEntrantFromEvent("sample_event_id", entrantText);
+
+                // remove from the UI
+                remove(entrantText);
+                notifyDataSetChanged();
+
+                // notifications for the organizers
+                Toast.makeText(getContext(), entrantText + " has been dropped.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return itemView;
     }
