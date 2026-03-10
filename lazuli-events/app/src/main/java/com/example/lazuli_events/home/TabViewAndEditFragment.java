6498f52cc4d6c3415decb4c70c1957c2bff23284
ceara <cearamahinay@gmail.com>
2026-03-09 20:59:00 -0600

    Fix a few imports

--- lazuli-events/app/src/main/java/com/example/lazuli_events/home/TabViewAndEditFragment.java
+++ lazuli-events/app/src/main/java/com/example/lazuli_events/home/TabViewAndEditFragment.java
@@ -1,4 +1,4 @@
-package com.example.lazuli_events;
+package com.example.lazuli_events.home;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.lazuli_events.R;
 
 
 public class TabViewAndEditFragment extends Fragment {
